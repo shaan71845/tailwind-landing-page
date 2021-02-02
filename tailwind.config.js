d11c9162053c0009709f3b1c27d56e0a449d7c4f
Shaan Alam <shaanalam369@gmail.com>
2021-02-02 15:24:31 +0530

    navbar and hero section

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,11 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    extend: {
+      screens: {
+        xsm: "375px",
+      },
+    },
     container: { center: true },
   },
   variants: {
